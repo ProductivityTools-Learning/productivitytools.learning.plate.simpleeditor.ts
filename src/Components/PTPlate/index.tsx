efd7913cdd3904351511b93e46ef5b19c378db82
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-22 22:47:04 +0200

    update and reset

--- src/Components/PTPlate/index.tsx
+++ src/Components/PTPlate/index.tsx
@@ -3,23 +3,26 @@ import { Plate, useResetPlateEditor } from "@udecode/plate";
 import { MyParagraphElement, MyValue } from "./typescript/plateTypes";
 
 const ResetEditorOnValueChange = ({ value }: { value: MyParagraphElement[] }) => {
-  console.log("ResetEditorOnValueChange")
+  console.log("ResetEditorOnValueChange");
+  console.log(value);
+  debugger;
   const resetPlateEditor = useResetPlateEditor();
   const isFirst = useRef(true);
   console.log("isFirst");
-  console.log(isFirst);
+  console.log(isFirst.current);
   useEffect(() => {
+    debugger;
     console.log("useEffect");
     console.log(isFirst);
-     
+
     if (isFirst.current) {
       console.log("isFirst.current");
-      console.log(isFirst);  
+      console.log(isFirst.current);
       isFirst.current = false;
       return;
     }
     resetPlateEditor();
-  }, [value, resetPlateEditor]);
+  }, [value, resetPlateEditor, isFirst]);
   console.log("return null");
 
   return null;
@@ -41,17 +44,25 @@ type Props<PTPlateProps> = {
 };
 
 function PTPlate<PTPlateProps>({ propValue }: Props<PTPlateProps>) {
-  const [debugValue, setDebugValue] = useState<MyParagraphElement[]>(propValue);
+  const [value, setValue] = useState<MyParagraphElement[]>(propValue);
+  const [resetValue, setResetValue] = useState<MyParagraphElement[]>(propValue);
 
   useEffect(() => {
     console.log("XXXXXXXXX");
-    setDebugValue(propValue);
+    setValue(propValue);
+    setResetValue(propValue);
   }, [propValue]);
+
+  const change = (e: MyParagraphElement[]) => {
+    setValue(e);
+  };
 
   return (
     <div>
-      <Plate<MyParagraphElement[]> editableProps={{ placeholder: "Type…" }} value={debugValue}>
-        <ResetEditorOnValueChange value={debugValue} />
+      PlateX2
+      {/* <Plate<MyParagraphElement[]> editableProps={{ placeholder: "Type…" }} value={debugValue}> */}
+      <Plate<MyParagraphElement[]> editableProps={{ placeholder: "Type…" }} value={value} onChange={change}>
+        <ResetEditorOnValueChange value={resetValue} />
       </Plate>
       {/* <span>Plate content:</span>
       <span>{JSON.stringify(content)}</span> */}
