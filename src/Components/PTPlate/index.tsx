861638cb418d3d34854b6a6aefcd12a7e6bfd187
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-23 09:03:26 +0200

    console log

--- src/Components/PTPlate/index.tsx
+++ src/Components/PTPlate/index.tsx
@@ -3,27 +3,25 @@ import { Plate, useResetPlateEditor } from "@udecode/plate";
 import { MyParagraphElement, MyValue } from "./typescript/plateTypes";
 
 const ResetEditorOnValueChange = ({ value }: { value: MyParagraphElement[] }) => {
-  console.log("ResetEditorOnValueChange");
-  console.log(value);
-  debugger;
+  // console.log("ResetEditorOnValueChange");
+  // console.log(value);
   const resetPlateEditor = useResetPlateEditor();
   const isFirst = useRef(true);
-  console.log("isFirst");
-  console.log(isFirst.current);
+  // console.log("isFirst");
+  // console.log(isFirst.current);
   useEffect(() => {
-    debugger;
-    console.log("useEffect");
-    console.log(isFirst);
+    // console.log("useEffect");
+    // console.log(isFirst);
 
     if (isFirst.current) {
-      console.log("isFirst.current");
-      console.log(isFirst.current);
+      // console.log("isFirst.current");
+      // console.log(isFirst.current);
       isFirst.current = false;
       return;
     }
     resetPlateEditor();
   }, [value, resetPlateEditor, isFirst]);
-  console.log("return null");
+ // console.log("return null");
 
   return null;
 };
@@ -40,18 +38,19 @@ const initialValue = (content: string) => [
 ];
 
 type Props<PTPlateProps> = {
-  propValue: MyParagraphElement[];
+  content: MyParagraphElement[];
 };
 
-function PTPlate<PTPlateProps>({ propValue }: Props<PTPlateProps>) {
-  const [value, setValue] = useState<MyParagraphElement[]>(propValue);
-  const [resetValue, setResetValue] = useState<MyParagraphElement[]>(propValue);
+function PTPlate<PTPlateProps>({ content }: Props<PTPlateProps>) {
+  const [value, setValue] = useState<MyParagraphElement[]>(content);
+  const [resetValue, setResetValue] = useState<MyParagraphElement[]>(content);
 
+  //if we use directly prop value, there was a delay in updating field when propValue changed 
+  //if we used value, the restet field was invoked every time when we started writing, which make writing not possible
   useEffect(() => {
-    console.log("XXXXXXXXX");
-    setValue(propValue);
-    setResetValue(propValue);
-  }, [propValue]);
+    setValue(content);
+    setResetValue(content);
+  }, [content]);
 
   const change = (e: MyParagraphElement[]) => {
     setValue(e);
